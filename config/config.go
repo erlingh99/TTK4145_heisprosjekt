b2f2a1e64f05b50589c6339f1533e252970c625a
Sigurd H Tullander <legotull.99@gmail.com>
2021-04-14 16:47:42 +0200

    Add network config

--- config/config.go
+++ config/config.go
@@ -3,3 +3,9 @@ package config
 
 var N_FLOORS = 3
 var N_BUTTONS = 3
+
+
+CONNECT_ADDR := "192.168.1.176"
+CONNECT_PORT := 20002
+
+LISTEN_PORT := 20002
--- main.go
+++ main.go
@@ -2,28 +2,37 @@ package main
 
 import (
 	"fmt"
+	"time"
 	"./config"
 	"./network/tcp"
 )
 
 func main() {
 	fmt.Println("Number of floors:", config.N_FLOORS);
+	recvChannel := make(chan int)
+	recvErrorChannel := make(chan error)
+	sendChannel := make(chan int)
+	sendErrorChannel := make(chan error)
 	fmt.Println("yo")
 	// IMPORTANT NOTE on how to use:
 	// You can only send messages from master to slave and not from slave to master
-	go tcp.Slave("10.100.23.167", 20001, errorChannel, testChannel)
-	// go tcp.Master(20001, errorChannel, testChannel)
+	go tcp.Slave(config.CONNECT_ADDR, config.CONNECT_PORT, recvErrorChannel, recvChannel)
+	go tcp.Master(config.LISTEN_PORT, sendErrorChannel, sendChannel)
 	fmt.Println("yo2")
 
 	for {
-		// testChannel <- 69
+		time.Sleep(1000 * time.Millisecond)
+		sendChannel <- 69
 		select {
-		case a := <-testChannel:
+		case a := <-recvChannel:
 			a = a
-			fmt.Println("Testmessage")
-		case a := <-errorChannel:
+			fmt.Println("Testmessage:", a)
+		case a := <-recvErrorChannel:
 			a = a
-			fmt.Println("Errormessage")
+			fmt.Println("Recieve errormessage:", a)
+		case a := <-sendErrorChannel:
+			a = a
+			fmt.Println("Send errormessage:", a)
 		}
 	}
 }
