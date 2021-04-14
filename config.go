d1d7c6da49bb5f2b90dfc5222ed1aa2a8348105f
bikleppe <bjorningelol@gmail.com>
2021-04-14 13:48:13 +0200

    Configfileadded

---
+++ config.go
@@ -0,0 +1,5 @@
+//Global constants for the elevator
+package config
+
+N_FLOORS = 3
+N_BUTTONS = 3
---
+++ elevatorManager/request.go
@@ -0,0 +1,6 @@
+import "../elevio/elevator_io.go"
+
+
+func request_above(Elevator e) {
+	for (f := e
+}
