fedbec996d6aa3c3620ea03bd7985454026b6654
Erling Hjermstad <erling-hjermstad@hotmail.com>
2021-04-15 15:55:41 +0200

    _

--- elevatorManager/fsm.go
+++ elevatorManager/fsm.go
@@ -7,12 +7,11 @@ import (
 	"elevatorproject/config"
 )
 
-
 var elevator Elevator
 
 func setAllLights(Elevator es) {
-	for (floor := 0; floor < N_FLOORS; floor++) {
-		for (btn := elevio.ButtonType(0); btn < 3; btn++) {
+	for floor := 0; floor < N_FLOORS; floor++ {
+		for btn := elevio.ButtonType(0); btn < 3; btn++ {
 			elevio.SetButtonLamp(b, f, true)
 		}
 	}
@@ -27,23 +26,21 @@ func fsm_onInitBetweenFloor() {
 func fsm_onRequestButtonPress(int floor, ButtonType btn) {
 	//ADD printing for debug
 
-	switch(elevator.behaviour) {
+	switch elevator.behaviour {
 	case EB_DoorOpen:
-		if (elevator.floor == floor) {
+		if elevator.floor == floor {
 			timer_start(config.DOOR_TIMEOUT)
-		}
-		else {
+		} else {
 			elevator.requests[floor][btn] = 1
 		}
 	case EB_Moving:
 		elevator.requests[floor][btn] = 1
 	case EB_Idle:
-		if (elevator.floor == floor) {
+		if elevator.floor == floor {
 			elevio.SetDoorOpenLamp(true)
 			timer_start(config.DOOR_TIMEOUT)
 			elevator.behaviour = EB_DoorOpen
-		}
-		else {
+		} else {
 			elevator.requests[floor][btn] = 1
 			elevator.dirn = request_chooseDirection(elevator)
 			elevio.MotorDirection(elevator.dirn)
@@ -61,9 +58,9 @@ func fsm_onFloorArrival(int newFloor) {
 	elevator.floor = newFloor
 	elevio.SetFloorIndicator(elevator.floor)
 
-	switch(elevator.behaviour) {
+	switch elevator.behaviour {
 	case EB_Moving:
-		if(request_shouldStop(elevator)) {
+		if request_shouldStop(elevator) {
 			elevio.SetMotorDirection(MD_Stop)
 			elevio.SetDoorOpenLamp(true)
 			elevator = request_clearAtCurrentFloor(elevator)
@@ -78,18 +75,17 @@ func fsm_onFloorArrival(int newFloor) {
 
 func fsm_onDoorTimeout() {
 
-	switch (elevator.behaviour) {
+	switch elevator.Behaviour {
 	case EB_DoorOpen:
 		elevator.dirn = request_chooseDirection(elevator)
 
 		elevio.SetDoorOpenLamp(false)
-		elevio.SetMotorDirection(elevator.drin)
+		elevio.SetMotorDirection(elevator.Dirn)
 
-		if (elevator.dirn == MD_Stop) {
-			elevator.behaviour = EB_Idle
-		}
-		else {
-			elevator.behaviour = EB_Moving
+		if elevator.Dirn == elevio.MD_Stop {
+			elevator.Behaviour = EB_Idle
+		} else {
+			elevator.Behaviour = EB_Moving
 		}
 	default:
 	}
