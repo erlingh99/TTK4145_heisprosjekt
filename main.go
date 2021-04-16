2754057be7e3ca85162b4479e9caa5ffee596656
Sigurd H Tullander <legotull.99@gmail.com>
2021-04-16 16:15:33 +0200

    Make structure for main

--- main.go
+++ main.go
@@ -12,8 +12,25 @@ import (
 )
 
 func main() {
-	elevio.Init("localhost:15657", config.N_FLOORS)
-	fmt.Println("Starting elevatorManager")
-	go elevatorManager.ElevatorManager()
-	for {}
+	// elevio.Init("localhost:15657", config.N_FLOORS)
+	// fmt.Println("Starting elevatorManager")
+	// go elevatorManager.ElevatorManager()
+	// for {}
+
+
+	// Create communication channels
+	chan1 := make(chan int)
+	chan2 := make(chan string)
+	chan3 := make(chan Order)
+	chan4 := make(chan ElevState)
+	chan5 := make(chan OrderHandlerState)
+
+	// Start orderHandler
+	go orderHandler.Init(chan1, chan2, chan3)
+
+	// Start elevatorManager
+	go elevatorManager.Init(chan4, chan5)
+
+	// Start networking
+	go networking.Init(chan1, chan2, chan3, chan4, chan5)
 }
